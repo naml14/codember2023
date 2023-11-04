e55b17a6b383b6e205a70a51f7a82c3a90e69795
Nelson Andrés Mora López <na2003ml.am@gmail.com>
2023-11-04 16:49:33 -0500

    Faltaba crear la respuesta en el formato correcto

--- reto-1.js
+++ reto-1.js
@@ -2,13 +2,19 @@ import * as fs from 'node:fs/promises';
 
 const file = "./message_01.txt";
 
-let words = {};
-
 try {
   const content = await fs.readFile(file, { encoding: 'utf8' })
   console.log(content)
-  content.toLowerCase().split(' ').forEach((word) => { words[word] = (words[word] || 0) + 1; })
+  let words = {};
+  content.replace("\n", '').toLowerCase().split(' ').forEach((word) => { words[word] = (words[word] || 0) + 1; })
   console.log(words);
+  let result = "";
+  let arr1 = Object.keys(words);
+  let arr2 = Object.values(words);
+  for (let i = 0; i < arr1.length; i++) {
+    result += arr1[i] + arr2[i];
+  }
+  console.log(result);
 } catch (error) {
   console.error(error)
 }
