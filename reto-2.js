a746c253e049144199806ba8d4526be536853129
Nelson Andrés Mora López <na2003ml.am@gmail.com>
2023-11-10 21:49:02 -0500

    Reto 2 de Codember 2023

---
+++ reto-2.js
@@ -0,0 +1,40 @@
+import * as fs from 'node:fs/promises';
+
+const file = "./message_02.txt";
+const init = 0;
+
+try {
+  const content = await fs.readFile(file, { encoding: 'utf8' })
+  console.log(content)
+  let steps = [];
+  steps = content.split('');
+  console.log(steps);
+  let result = init;
+  let submit = "";
+  for (let i = 0; i < steps.length; i++) {
+    switch (steps[i]) {
+      case '#':
+        result += 1;
+        break;
+
+      case '@':
+        result -= 1;
+        break;
+
+      case '*':
+        result *= result;
+        break;
+
+      case '&':
+        console.log(result);
+        submit += result.toString();
+        break;
+
+      default:
+        break;
+    }
+  }
+  console.log(submit);
+} catch (error) {
+  console.error(error)
+}
